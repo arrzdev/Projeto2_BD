aa493132f58a67894d807997066bd440825ce5a4
arrzdev <andre.arroz.santos@gmail.com>
2023-06-16 21:12:55 +0100

    Orders working / ids generating

--- app/app.py
+++ app/app.py
@@ -14,6 +14,7 @@ from psycopg.rows import namedtuple_row
 from psycopg_pool import ConnectionPool
 
 from random import randint
+from datetime import datetime
 
 
 # postgres://{user}:{password}@{hostname}:{port}/{database-name}
@@ -115,8 +116,13 @@ def customers_index():
 # ADD CUSTOMER
 @app.route("/customers/add", methods=["GET"])
 def customers_add():
-  #/customers/add?name=joana&email=joana@wtf.pt&phone=969696969&adress=Manhica
-  cust_no = randint(0, 999999)
+  #get new customer id
+  query = "SELECT MAX(cust_no) FROM customer"
+  args = ()
+
+  last_cust_no = execute_query(query, args, action=FETCH_ONE)
+
+  cust_no = last_cust_no.max + 1 if last_cust_no.max is not None else 1
   name = request.args.get("name", "debug")
   email = request.args.get("email", f"debug-{cust_no}@tecnico.ulisboa.pt")
   phone = request.args.get("phone", "969696969")
@@ -143,29 +149,57 @@ def customers_remove():
   delete_all = int(request.args.get("all", 0))
   customer_ids = request.args.get("ids", "")
 
-  # if customer_ids is False and customer_ids == []:
-  #   flash("Please select a customer to remove")
-  #   return redirect(url_for("customers_index"))
+  # Start a transaction
+  with pool.connection() as conn:
+    with conn.cursor() as cur:
+      try:
+        # if delete_all is true, delete all customers
+        if delete_all == 1:
+          # Delete all payments first
+          delete_payments_query = "DELETE FROM pay"
+          cur.execute(delete_payments_query)
 
-  #if delete_all is true, delete all customers
-  if delete_all == 1:
-    query = "DELETE FROM customer"
-    args = ()
-  else:
-    #parse ids
-    customer_ids = tuple(map(int, customer_ids.split(",")))
-    placeholders = ", ".join(["%s"] * len(customer_ids))
+          # Delete associated contains first
+          delete_contains_query = "DELETE FROM contains"
+          cur.execute(delete_contains_query)
 
-    query = f"DELETE FROM customer WHERE cust_no IN ({placeholders})"
-    args = customer_ids
+          # Delete associated orders first
+          delete_orders_query = "DELETE FROM orders"
+          cur.execute(delete_orders_query)
 
-  #execute query
-  sucessfull = execute_query(query, args, action=INSERT_REMOVE)
+          # Then delete all customers
+          query = "DELETE FROM customer"
+          cur.execute(query)
+        else:
+          # parse ids
+          customer_ids = tuple(map(int, customer_ids.split(",")))
+          placeholders = ", ".join(["%s"] * len(customer_ids))
 
-  # if not sucessfull:
-  #   flash("Error deleting customer(s)")
-  
-  #render main_page
+          #delete associated payments for the selected customers
+          delete_payments_query = f"DELETE FROM pay WHERE cust_no IN ({placeholders})"
+          cur.execute(delete_payments_query, args)
+
+          # Delete associated contains for the selected customers
+          delete_contains_query = f"DELETE FROM contains WHERE cust_no IN ({placeholders})"
+          cur.execute(delete_contains_query, args)
+
+          # Delete associated orders for the selected customers
+          delete_orders_query = f"DELETE FROM orders WHERE cust_no IN ({placeholders})"
+          cur.execute(delete_orders_query, args)
+
+          # Then delete the selected customers
+          query = f"DELETE FROM customer WHERE cust_no IN ({placeholders})"
+          args = customer_ids
+          cur.execute(query, args)
+
+        # Commit the transaction
+        conn.commit()
+      except:
+        log.exception("Error deleting customer")
+        # Rollback the transaction
+        conn.rollback()
+
+  # Render main_page
   return redirect(url_for("customers_index"))
 
 ########################################################
@@ -237,6 +271,33 @@ def products_remove():
   #   return redirect(url_for("products_index"))
 
   #if delete_all is true, delete all customers
+  # Start a transaction
+  with pool.connection() as conn:
+    with conn.cursor() as cur:
+      try:
+        # if delete_all is true, delete all customers
+        if delete_all == 1:
+          #delete associated supliers first
+          delete_supplies_query = "DELETE FROM supplier"
+          cur.execute(delete_supplies_query)
+          
+          query = "DELETE FROM product"
+          args = ()
+          cur.execute(query)
+        else:
+          #parse ids
+          product_ids = tuple(product_ids.split(","))
+          placeholders = ", ".join(["%s"] * len(product_ids))
+
+          query = f"DELETE FROM product WHERE sku IN ({placeholders})"
+          args = product_ids
+
+        # Commit the transaction
+        conn.commit()
+      except:
+        log.exception("Error deleting customer")
+        # Rollback the transaction
+        conn.rollback()
   if delete_all == 1:
     query = "DELETE FROM product"
     args = ()
@@ -368,11 +429,126 @@ def suppliers_remove():
   #render main_page
   return redirect(url_for("suppliers_index"))
 
+########################################################
 
-@app.route("/ping", methods=("GET",))
-def ping():
-  log.debug("ping!")
-  return jsonify({"message": "pong!", "status": "success"})
+#MANAGE ORDER
+@app.route("/orders", methods=["GET"])
+def orders_index():
+  #get filter arg from request
+  page = int(request.args.get("page", 1))
+
+  #run query without filter
+  query = "SELECT o.*, CASE WHEN p.order_no IS NOT NULL THEN 'Paid' ELSE 'Waiting for payment' END AS paid FROM orders o LEFT JOIN pay p ON o.order_no = p.order_no;"
+  args = ()
+
+  orders = execute_query(query, args, action=FETCH_ALL)
+
+  #display 8 products per page
+  max_page = get_max_page(len(orders), 8)
+  orders = orders[(page-1)*8:page*8]
+
+  return render_template(
+    "/orders/index.html",
+    navbar_text="Orders", 
+    search_available=False,
+    current_page=page,
+    max_page=max_page,
+    search_label="Search by order_no or cust_no",
+    orders=orders
+  )
+
+
+# ORDER REROUTE
+@app.route("/order", methods=["GET"])
+def order_index():
+  customers = execute_query("SELECT * FROM customer", (), action=FETCH_ALL)
+
+  order_no = request.args.get("order_no", "")
+  if order_no:
+    #get cust_no
+    cust_no = request.args.get("cust_no", "")
+
+    #get order contains
+    query = "SELECT p.sku, p.name, p.description, p.price, p.ean, c.qty FROM contains c JOIN product p ON c.sku = p.sku WHERE c.order_no = %s;"
+    args = (order_no,)
+    order = execute_query(query, args, action=FETCH_ALL)
+
+    return render_template("/order/index.html", 
+      navbar_text="Order - Payment",
+      search_available=False,
+      order_exists=True,
+      order=order,
+      order_no=order_no,
+      cust_no=cust_no
+    )
+
+  skus = request.args.get("skus", "")
+  skus = tuple(skus.split(","))
+
+  names = request.args.get("names", "")
+  names = tuple(names.split(","))
+
+  descriptions = request.args.get("descriptions", "")
+  descriptions = tuple(descriptions.split(","))
+
+  prices = request.args.get("prices", "")
+  prices = tuple(prices.split(","))
+
+  eans = request.args.get("eans", "")
+  eans = tuple(eans.split(","))
+
+  order = list(zip(skus, names, descriptions, prices, eans))
+
+  return render_template("/order/index.html",
+    navbar_text="Order Placing", 
+    search_available=False, 
+    order_exist=False,
+    order=order,
+    customers=customers
+  )
+
+@app.route("/order/place", methods=["GET"])
+def order_place():
+  #current date in format YYYY-MM-DD
+  date = datetime.now().strftime("%Y-%m-%d")
+  
+  # create order row
+  latest_order = execute_query("SELECT MAX(order_no) FROM orders", (), action=FETCH_ONE)
+  order_no = latest_order[0] + 1 if latest_order[0] else 1
+  cust_no = request.args.get("cust_no", 1)
+  
+  skus = request.args.get("skus", "")
+  skus = tuple(skus.split(","))
+
+  quantities = request.args.get("quantities", "")
+  quantities = tuple(quantities.split(","))
+
+  #insert into orders table
+  query = "INSERT INTO orders (order_no, cust_no, date) VALUES (%s, %s, %s)"
+  args = (order_no, cust_no, date)
+  sucessfull = execute_query(query, args, action=INSERT_REMOVE)
+
+  #insert into contains
+  for sku, quantity in zip(skus, quantities):
+    query = "INSERT INTO contains (order_no, sku, qty) VALUES (%s, %s, %s)"
+    args = (order_no, sku, quantity)
+    sucessfull = execute_query(query, args, action=INSERT_REMOVE)
+
+  #render main_page
+  return redirect(url_for("order_index", order_no=order_no, cust_no=cust_no))
+
+@app.route("/order/pay", methods=["GET"])
+def order_pay():
+  order_no = request.args.get("order_no", "")
+  cust_no = request.args.get("cust_no", "")
+  
+  #insert in pay
+  query = "INSERT INTO pay (order_no, cust_no) VALUES (%s, %s)"
+  args = (order_no, cust_no)
+  sucessfull = execute_query(query, args, action=INSERT_REMOVE)
+
+  #render main_page
+  return redirect(url_for("orders_index"))
 
 
 if __name__ == "__main__":
